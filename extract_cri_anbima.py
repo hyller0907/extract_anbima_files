fbbf146e158b047888e0a0e8cca204cdcffd8dba
hyller vianna <hv.vianna@gmail.com>
2021-06-30 14:46:40 -0300

    Script de extração do arquivo MTM do site da ANBIMA
    O arquivo extract_cri_anbima.py realiza a extração da planilha
    em EXCEL, dos preços de CRI e CRA diretamente do site da anbima.
    
    O xls extraído é nomeado com a data em que ele foi extraído.

---
+++ extract_cri_anbima.py
@@ -0,0 +1,30 @@
+#!/home/hvianna/anaconda3/bin/python
+
+
+'''
+BAIXANDO AS INFORMAÇÕS DAS TAXAS CRI DO SITE DA ANBIMA
+https://www.anbima.com.br/pt_br/informar/precos-e-indices/precos/precos.htm
+
+'''
+import pandas as pd
+from datetime import date
+import requests
+import io
+
+today = date.today()
+file_name_compl = today.strftime("%Y%m%d")
+
+with requests.Session() as s:
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0'}
+    
+    url = 'https://www.anbima.com.br/pt_br/anbima/TaxasCriCraExport/exportarCSV?filtroTermo=&filtroData='
+    r = s.get(url, headers = headers).content
+    
+    arquivo = pd.read_csv(io.StringIO(r.decode('ISO-8859-1')), sep=";")
+    
+    my_file = f'ANBIMA_TAXAS_CRI-CRA_{file_name_compl}.xlsx'
+    arquivo.to_excel(my_file, index=False)
+          
+s.close()
+print('Extração concluida')
